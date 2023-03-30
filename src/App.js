197f38c833310559e5baecd600b57f729a18bf44
Jose Mejia <jose.mejia04@gmail.com>
2023-03-30 16:09:06 -0400

    added routing for authenication components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { AuthContextProvider } from "./context/AuthContext";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         {/* dynamic component based routing  */}
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/account" element={<Account />} />
         </Routes>
       </AuthContextProvider>
     </>
