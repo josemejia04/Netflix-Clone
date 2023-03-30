3130d13564b6e96d98622797fe4d82c5f8f4930f
Jose Mejia <jose.mejia04@gmail.com>
2023-03-30 15:57:28 -0400

    created authenication functionality and wrapped app components for context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { AuthContextProvider } from "./context/AuthContext";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      {/* wrapped components to subscribe to context changes for user authenication */}
+      <AuthContextProvider>
+        <Navbar />
+        {/* dynamic component based routing  */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </AuthContextProvider>
     </>
   );
 }
