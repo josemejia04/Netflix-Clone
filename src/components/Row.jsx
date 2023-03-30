fb20a85642eadd80d4b393ad7306ae4689f48a52
Jose Mejia <jose.mejia04@gmail.com>
2023-03-30 14:56:34 -0400

    added slider functionality to Row component

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,9 +1,10 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Movie from "./Movie";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 //props recieved from Home component
-const Row = ({ title, fetchURL }) => {
+const Row = ({ rowId, title, fetchURL }) => {
   //set movie state to empty array using useState hook
   const [movies, setMovies] = useState([]);
 
@@ -20,19 +21,44 @@ const Row = ({ title, fetchURL }) => {
   //console log reponse data array
   //console.log(movies);
 
+  //event listener function to manipulate DOM slider elements to scroll when clicked using the unique id for each row
+  const slideLeft = () => {
+    let slider = document.getElementById("slider" + rowId);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+
+  //event listener function to manipulate DOM slider elements to scroll when clicked using the unique id for each row
+  const slideRight = () => {
+    let slider = document.getElementById("slider" + rowId);
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
   return (
     <>
+      {/* title object */}
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
-      <div className="relative flex items-center">
+      <div className="relative flex items-center group">
+        {/* onClick react-icon to execute event listener slide function to the left */}
+        <MdChevronLeft
+          onClick={slideLeft}
+          size={40}
+          className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+        />
         <div
-          id={"slider"}
+          id={"slider" + rowId}
           className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
         >
-          {/* .map function to create a new array populated with the results from movies api call and a key to identify which items have changed in the array */}
+          {/* map function to create a new array populated with the results from the movies api call state and a key to identify which items have changed in the array */}
           {movies.map((item, id) => (
             <Movie key={id} item={item} />
           ))}
         </div>
+        {/* onClick react-icon to execute event listener slide function to the right */}
+        <MdChevronRight
+          onClick={slideRight}
+          size={40}
+          className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+        />
       </div>
     </>
   );
