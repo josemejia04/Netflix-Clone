c079fafeb901fd192ad1ff5857bf404e8941bea9
Jose Mejia <jose.mejia04@gmail.com>
2023-03-30 11:24:50 -0400

    added home page, dynamic routing and API endpoints

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Main = () => {
+    //set movie state to empty array
+const [movies, setMovies] = useState([]);
+
+
+  return (
+    <div>Main</div>
+  )
+}
+
+export default Main
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  //imported BrowserRouter to allow for components to be navigated as multiple pages with different urls
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
